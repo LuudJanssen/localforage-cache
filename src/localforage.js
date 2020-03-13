c93c03389de492e6752a3e569b85b0a53864cb1f
Luud Janssen <luud-@live.nl>
2020-03-13 16:21:09 +0100

    Build basic implementation by adding expires keys

--- src/localforage.js
+++ src/localforage.js
@@ -1,13 +1,96 @@
 import localforage from "localforage"
+import Sha256 from "sha.js"
 
-class LocalForageCache {
-  localforageInstance = localforageInstance
-  config = localforage.config
-  defineDriver = localforage.defineDriver
-  driver = localforage.driver
-  getDriver = localforage.getDriver
-  getSerializer = localforage.getSerializer
-  ready = localforage.ready
-  setDriver = localforage.setDriver
-  supports = localforage.supports
+export class LocalForageCache {
+  constructor(options) {
+    this.storage = localforage.createInstance()
+    this.defineDriver = this.storage.defineDriver
+    this.driver = this.storage.driver
+    this.getDriver = this.storage.getDriver
+    this.getSerializer = this.storage.getSerializer
+    this.ready = this.storage.ready
+    this.setDriver = this.storage.setDriver
+    this.supports = this.storage.supports
+    this.clear = this.storage.clear
+
+    this.config(options)
+  }
+
+  async setItem(key, value, expires = this.storage._config.defaultExpiration) {
+    const expirationKey = this._expirationKey(key)
+
+    console.log("expiration key", expirationKey)
+
+    await this.storage.setItem(key, value)
+    return this.storage.setItem(expirationKey, expires)
+  }
+
+  async getItem(key) {
+    const expirationKey = this._expirationKey(key)
+    const expires = await this.storage.getItem(expirationKey)
+
+    console.log("expiration result", expires)
+
+    if (expires === null) {
+      return this.storage.getItem(key)
+    }
+
+    const hasExpired = this._hasExpired(expires)
+
+    if (hasExpired) {
+      await this.removeItem(key)
+      return null
+    }
+
+    return this.storage.getItem(key)
+  }
+
+  async removeItem(key) {
+    const expirationKey = this._expirationKey(key)
+    const removeValue = this.storage.removeItem(key)
+    const removeExpiration = this.storage.removeItem(expirationKey)
+
+    return Promise.all([removeValue, removeExpiration])
+  }
+
+  config(options) {
+    if (typeof options !== "object") {
+      return this.storage.config(options)
+    }
+
+    const { defaultExpiration } = options
+
+    if (typeof defaultExpiration === "undefined") {
+      options.defaultExpiration = Infinity
+    }
+
+    return this.storage.config(options)
+  }
+
+  createInstance(options) {
+    return new LocalForageCache(options)
+  }
+
+  _expirationKey(key) {
+    const hash = new Sha256()
+      .update(key)
+      .digest("hex")
+      .substring(0, 8)
+
+    return `${key}_expires_${hash}`
+  }
+
+  _hasExpired(expires) {
+    if (expires === Infinity) {
+      return false
+    }
+
+    if (expires instanceof Date === false) {
+      return false
+    }
+
+    return expires.getTime() < Date.now()
+  }
 }
+
+export default new LocalForageCache()
